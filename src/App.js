5321d8cfbe3f278987e6be164baf168f8669a911
olliebrownlow <olliebrownlow@gmail.com>
2019-09-25 12:02:57 +0100

    calls api and updates state with the response data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
+const API_URL = 'http://jsonplaceholder.typicode.com';
 
 class App extends Component {
+  state = {
+    users: []
+  }
+
+  componentDidMount() {
+    const url = `${API_URL}/users/`;
+    axios.get(url)
+    .then(response => response.data)
+    .then((data) => {
+      this.setState({ users: data})
+      console.log(this.state.users)
+    })
+  }
+
   render() {
     return (
        <div className="container">
