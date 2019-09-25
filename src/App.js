74d00c5c28a84acab513d820407ce47f3db57d32
olliebrownlow <olliebrownlow@gmail.com>
2019-09-25 12:23:06 +0100

    uses response data to publish info in state in the app

--- src/App.js
+++ src/App.js
@@ -23,11 +23,13 @@ class App extends Component {
        <div className="container">
         <div className="col-xs-8">
         <h1>React Axios Example</h1>
+        { this.state.users.map((user) => (
           <div className="card">
             <div className="card-body">
-              <h5 className="card-title">My User</h5>
+              <h5 className="card-title">{user.name}</h5>
             </div>
           </div>
+        ))}
         </div>
        </div>
     );
